51d0850aefe26f4c675f3452ff92d863faab5bd0
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-04-01 13:50:21 +0600

    header section created . also do responsive

--- src/Componentes/SharedComponents/Header/Header.jsx
+++ src/Componentes/SharedComponents/Header/Header.jsx
@@ -1,56 +1,66 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Header.css";
 
-function Header() {
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="mx-5 my-5 bg-transparent">
-      <div className="flex justify-between items-center">
-        {/* Logo with link to home */}
-        <Link to="/" className="">
-          <img
-            src="https://i.ibb.co.com/B5zSn3LC/Screenshot-410-removebg-preview.png"
-            alt="WoodenWhisper Logo"
-            className="w-[100px] h-[70px] bg-cover"
-          />
-        </Link>
-
-        {/* Navigation */}
-        <nav className="">
-          <ul className="flex space-x-10 nav_Links">
-            <li className="group relative">
-              <Link to="/about" className="relative">
-                About
-                <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-            <li className="group relative">
-              <Link to="/journal" className="relative">
-                Journal
-                <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-            <li className="group relative">
-              <Link to="/shop" className="relative">
-                Shop
-                <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-            <li className="group relative">
-              <Link to="/videos" className="relative">
-                Videos
-                <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-            <li className="group relative">
-              <Link to="/projects" className="relative">
-                Projects
-                <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center fixed top-0 w-full z-50">
+      {/* Logo */}
+      <div className="logo">
+        <img
+          src="https://i.ibb.co/B5zSn3LC/Screenshot-410-removebg-preview.png"
+          alt="Logo"
+          className="h-20 w-25"
+        />
       </div>
+
+      {/* Hamburger Menu */}
+      <button
+        className="md:hidden text-gray-700 focus:outline-none z-50 relative"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`hamburger ${isOpen ? "open" : ""}`}
+        ></div>
+      </button>
+
+      {/* Navigation Links */}
+      <nav
+        className={`nav-links fixed top-0 left-0 w-full h-screen bg-white flex flex-col items-center justify-center transition-all duration-500 ease-in-out transform ${
+          isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+        } md:static md:w-auto md:h-auto md:bg-transparent md:flex md:translate-y-0 md:opacity-100`}
+      >
+        <ul className="flex flex-col md:flex-row w-full text-center gap-6 nav_Links">
+          <li>
+            <a href="#projects" className="nav-link hover-underline block py-4 ">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#shop" className="nav-link hover-underline block py-4 ">
+              Shop
+            </a>
+          </li>
+          <li>
+            <a href="#video" className="nav-link hover-underline block py-4 ">
+              Video
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-link hover-underline block py-4 ">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#journals" className="nav-link hover-underline block py-4 ">
+              Journals
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
